016e369057547e97fb43daae131d8375428d17b0
Sean Wernimont <swernimo@users.noreply.github.com>
2020-07-28 20:36:30 -0600

    returning JSON collection for get collection

---
+++ GameComparisonAPI/Entities/CollectionItem.cs
@@ -0,0 +1,35 @@
+﻿using System.Xml.Linq;
+
+namespace GameComparisonAPI.Entities
+{
+    public class CollectionItem
+    {
+        public string ObjectType { get; set; }
+        public int Id { get; set; }
+        public string SubType { get; set; }
+        public string Name { get; set; }
+        public int YearPublished { get; set; }
+        public string ImageUrl { get; set; }
+        public string ThumbnailUrl { get; set; }
+        public bool Owned { get; set; }
+        public int NumberPlays { get; set; }
+
+        public static CollectionItem ParseItem (XElement el)
+        {
+            var item = new CollectionItem
+            {
+                ObjectType = el.Attribute("objecttype").Value,
+                Id = int.Parse(el.Attribute("objectid").Value),
+                SubType = el.Attribute("subtype").Value,
+                Name = el.Element("name").Value,
+                YearPublished = int.Parse(el.Element("yearpublished").Value),
+                ImageUrl = el.Element("image").Value,
+                ThumbnailUrl = el.Element("thumbnail").Value,
+                Owned = (int.Parse(el.Element("status").Attribute("own").Value) == 1),
+                NumberPlays = int.Parse(el.Element("numplays").Value)
+            };
+
+            return item;
+        }
+    }
+}
---
+++ GameComparisonAPI/GetCollection.cs
@@ -0,0 +1,47 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Net;
+using Polly;
+using System.Xml;
+using GameComparisonAPI.Entities;
+using System.Collections.Generic;
+using System.Xml.Linq;
+
+namespace GameComparisonAPI
+{
+    public static class GetCollection
+    {
+        static readonly HttpClient client = new HttpClient();
+
+        [FunctionName("GetCollection")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "GetCollection/{username}")] HttpRequest req, string username,
+            ILogger log)
+        {
+            log.LogInformation($"Trying to get collection for {username}");
+            var collection = new List<CollectionItem>();
+
+            Policy.HandleResult<HttpResponseMessage>(r => r.StatusCode == HttpStatusCode.OK).RetryForever();
+
+            var response = await client.GetAsync($"https://www.boardgamegeek.com/xmlapi2/collection/?username={username}&subtype=boardgame");
+
+            var str = await response.Content.ReadAsStringAsync();
+            var doc = XDocument.Parse(str);
+
+            foreach(var el in doc.Elements().Nodes())
+            {
+                collection.Add(CollectionItem.ParseItem((XElement)el));
+            }
+
+            return new OkObjectResult(collection);
+        }
+    }
+}
