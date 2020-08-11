22eaf114e73e37769485c233a4adaa8d64f78856
Sean Wernimont <swernimo@users.noreply.github.com>
2020-08-11 10:27:04 -0600

    getting db connection string from app settings

--- GameComparisonAPI/SearchByUPC.cs
+++ GameComparisonAPI/SearchByUPC.cs
@@ -7,20 +7,28 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using System.Collections.Generic;
 using GameComparisonAPI.Entities;
+using Microsoft.Extensions.Configuration;
 
 namespace GameComparisonAPI
 {
     public static class Search
     {
+        private static IConfigurationRoot _config;
+
         [FunctionName("SearchByUPC")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", Route = "SearchByUPC/{code}")] HttpRequest req, string code,
-            ILogger log)
+            ILogger log, ExecutionContext context)
         {
             log.LogInformation($"Searching for barcode: {code}");
+            _config = new ConfigurationBuilder()
+            .SetBasePath(context.FunctionAppDirectory)
+            .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
+            .AddEnvironmentVariables()
+            .Build();
             var infoList = new List<SearchResults>();
 
-            using (var connection = new SqlConnection(""))
+            using (var connection = new SqlConnection(_config["DBConnectionString"]))
             {
                 connection.Open();
                 using (var command = new SqlCommand("select Id, Title, imageUrl from Game where barcode = @barcode", connection))
